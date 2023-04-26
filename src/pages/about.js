731c96e40ea1cd2ac1637aae3c25e1df3912b272
Jesse Krensel <jdkrensel@gmail.com>
2023-04-26 18:24:32 -0400

    feat: add components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar.jsx";
+import ContactForm from "@/components/ContactForm/ContactForm.jsx";
+
 import NavFooter from "@/components/Footers/NavFooter.jsx";
 import CopyrightFooter from "@/components/Footers/CopyrightFooter.jsx";
 
@@ -9,7 +11,14 @@ export default function about() {
             <header>
                 <NavBar />
             </header>
-            <main className="grow flex flex-col justify-around"></main>
+            <main className="grow flex flex-col">
+                <h1>
+                    What is Serenity Home Repair? What is the mission of
+                    Serenity Home Repair? What is the background of serenity
+                    Home repair? Why should I hire serenity home repair?
+                </h1>
+                <ContactForm />
+            </main>
             <footer>
                 <NavFooter />
                 <CopyrightFooter />
