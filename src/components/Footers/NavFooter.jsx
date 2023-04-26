0d2f921162c09b35e2ccf90dd995e452f965f4e6
Jesse Krensel <jdkrensel@gmail.com>
2023-04-26 18:23:48 -0400

    feat: new component

--- src/components/Footers/NavFooter.jsx
+++ src/components/Footers/NavFooter.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function NavFooter() {
-  return (
-    <div className="bg-slate-500">NavFooter</div>
-  )
+    return (
+        <div className="bg-slate-500">
+            <div className="container mx-auto flex justify-around">
+                <Image src="" alt="Serenity Home Repair Logo" />
+                <ul>
+                    {["Home", "About", "Services", "Work", "Contact"].map(
+                        (page, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link href={`/${page.toLowerCase()}`}>
+                                        {page}
+                                    </Link>
+                                </li>
+                            );
+                        }
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
 }
