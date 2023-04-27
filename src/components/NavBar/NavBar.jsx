37bb45f4d1ea76b06704262cce81434b55ffe767
Jesse Krensel <jdkrensel@gmail.com>
2023-04-27 14:38:36 -0400

    extract markup into components

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,21 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import MenuButton from "./MenuButton.jsx";
+import Logo from "./Logo.jsx";
+import NavLinks from "./NavLinks.jsx";
+import RequestQuoteBtn from "@/components/shared/RequestQuoteBtn.jsx";
 
 export default function NavBar() {
-    const pages = ["About", "Services", "Work", "Contact"];
-    const pagesListItems = pages.map((page, index) => {
-        return (
-            <li key={index}>
-                <Link href={`/${page.toLowerCase()}`}>{page}</Link>
-            </li>
-        );
-    });
     return (
-        <nav className="bg-slate-500">
-            <div className="container mx-auto flex justify-between">
-                <Link href="/">
-                    <Image src="" alt="Serenity Home Repair" />
-                </Link>
-                <ul className="flex">{pagesListItems}</ul>
-                <div>
-                    <p>610-613-4832</p>
-                    <button>Request Quote</button>
+        <nav className="bg-gray-200 drop-shadow-md">
+            <div className="container mx-auto flex justify-between items-center p-4 gap-4">
+                <div className="flex items-center gap-4">
+                    <MenuButton />
+                    <Logo />
+                </div>
+                <NavLinks />
+                <div className="flex flex-col gap-2 text-center">
+                    <p className="font-bold text-gray-700 text-sm">610-613-4832</p>
+                    <RequestQuoteBtn text="Request Quote" />
                 </div>
             </div>
         </nav>
