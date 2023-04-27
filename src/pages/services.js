23f8d8be8ac640576d93973ef9ef1905546bd4b0
Jesse Krensel <jdkrensel@gmail.com>
2023-04-27 19:00:03 -0400

    style: add sticky header and section heading

--- src/pages/services.js
+++ src/pages/services.js
@@ -8,10 +8,11 @@ import CopyrightFooter from "@/components/Footers/CopyrightFooter.jsx";
 export default function services() {
     return (
         <>
-            <header>
+            <header className="sticky top-0 z-40">
                 <NavBar />
             </header>
             <main className="grow flex flex-col">
+                <h1 className="text-3xl font-bold text-gray-800 text-center">Services</h1>
                 <ServicesCards />
                 <ContactForm />
             </main>
