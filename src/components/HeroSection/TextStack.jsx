ee0a49ed34f8e969c456df66360cf21313f8c57f
Jesse Krensel <jdkrensel@gmail.com>
2023-04-27 18:55:52 -0400

    add link to button

--- src/components/HeroSection/TextStack.jsx
+++ src/components/HeroSection/TextStack.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 export default function TextStack() {
     return (
         <div className="flex flex-col gap-4 text-center max-w-[22rem]">
             <h1 className="text-3xl text-gray-800 font-bold">
-                Discover <span className="underline decoration-sy-700">peace of mind</span> in your home with <span className="whitespace-nowrap">Serenity Home Repair</span>.
+                Discover{" "}
+                <span className="underline decoration-sy-700">
+                    peace of mind
+                </span>{" "}
+                in your home with{" "}
+                <span className="whitespace-nowrap">Serenity Home Repair</span>.
             </h1>
             <p className="text-xl text-gray-600">
                 We handle all your home repair needs so you don&apos;t have to.
@@ -14,7 +20,7 @@ export default function TextStack() {
                 type="button"
                 className="font-bold text-gray-800 bg-sy-200 px-4 py-2 mt-6 rounded-sm w-36 mx-auto"
             >
-                View Services
+                <Link href="/services">View Services</Link>
             </button>
         </div>
     );
