81b1d59bdadccc2635f0843a31b869705b12cdbb
Jesse Krensel <jdkrensel@gmail.com>
2023-04-27 14:38:05 -0400

    extract markup into components

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,29 +1,27 @@
 import React from "react";
+import TextStack from "./TextStack.jsx";
+import FormHeading from "./FormHeading.jsx";
 import RequestForm from "@/components/RequestForm/RequestForm.jsx";
+import hero from "/public/assets/hero.jpg";
+import Image from "next/image";
 
 export default function HeroSection() {
     return (
-        <div className="container mx-auto flex justify-around">
-            <div>
-                <h1>
-                    Say goodbye to home repair stress and hello to serenity with
-                    Serenity Home Repair
-                </h1>
-                <p>
-                    We handle all your home repair needs so you don&apos;t have
-                    to
-                </p>
-                <button>View Services</button>
-            </div>
-            <div>
-                <div className="flex flex-col text-center">
-                    <h2>Turn your house into a home.</h2>
-                    <p>
-                        Request a complimentary quote by securely submitting
-                        your information.
-                    </p>
+        <div className="relative">
+            <Image
+                src={hero}
+                alt="Handyman image"
+                className="object-cover"
+                style={{ filter: "brightness(1.5) opacity(0.25)" }}
+                fill
+            />
+
+            <div className="container mx-auto flex flex-col sm:flex-row justify-around items-center p-4 gap-24 sm:gap-8 relative my-16">
+                <TextStack />
+                <div className="border-2 border-gray-400 rounded-md px-6 py-8 bg-gray-50 drop-shadow-md basis-10/12 max-w-[20rem] sm:min-w-[20rem]">
+                    <FormHeading />
+                    <RequestForm />
                 </div>
-                <RequestForm />
             </div>
         </div>
     );
