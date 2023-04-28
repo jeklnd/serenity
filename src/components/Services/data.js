9a841a1d795fdc588d3e5490968f1c4d94bfa1a5
Jesse Krensel <jdkrensel@gmail.com>
2023-04-28 18:51:19 -0400

    add data

---
+++ src/components/Services/data.js
@@ -0,0 +1,90 @@
+export const rooms = [
+    {
+        room: "Bedroom",
+        image: "https://picsum.photos/200",
+        text: "lorem ipsum gerby floopy flipping fool wagon garbunkle do diagrama gonal tire teeker flippy flick flook flock",
+    },
+    {
+        room: "Bathroom",
+        image: "https://picsum.photos/200",
+        text: "lorem ipsum gerby floopy flipping fool wagon garbunkle do diagrama gonal tire teeker flippy flick flook flock",
+    },
+    {
+        room: "Kitchen",
+        image: "https://picsum.photos/200",
+        text: "lorem ipsum gerby floopy flipping fool wagon garbunkle do diagrama gonal tire teeker flippy flick flook flock",
+    },
+    {
+        room: "Basement",
+        image: "https://picsum.photos/200",
+        text: "lorem ipsum gerby floopy flipping fool wagon garbunkle do diagrama gonal tire teeker flippy flick flook flock",
+    },
+];
+
+export const services = [
+    {
+        image: "https://picsum.photos/200",
+        name: "Home Repair and Maintenance",
+        listItems: [
+            "General repairs",
+            "Door repair and replacement",
+            "Molding repair and replacement",
+            "Fence repair and replacement",
+            "Fixture replacement",
+            "Light fixture replacement",
+            "Fan replacement",
+            "Carpentry",
+            "Flooring repair and replacement",
+            "Drywall repair",
+            "Painting",
+            "Furniture assembly and repair",
+        ],
+    },
+    {
+        image: "https://picsum.photos/200",
+        name: "Plumbing",
+        listItems: [
+            "Repair and replacement of faucets, sinks, and toilets",
+            "Shower and tub repair and replacement",
+            "Pipe repair and replacement",
+            "Water heater repair and replacement",
+            "Drain cleaning",
+            "Water pressure issues",
+        ],
+    },
+    {
+        image: "https://picsum.photos/200",
+        name: "Electrical",
+        listItems: [
+            "Outlet and switch replacement",
+            "Wiring repair and replacement",
+            "Circuit breaker replacement",
+            "Lighting installation and repair",
+        ],
+    },
+    {
+        image: "https://picsum.photos/200",
+        name: "Bathroom and Kitchen Remodeling",
+        listItems: [
+            "Vanity and sink installation and replacement",
+            "Bathtub and shower installation and replacement",
+            "Fixture installation and replacement",
+            "Backsplash installation",
+            "Cabinet repair and replacement",
+            "Countertop repair and replacement",
+            "Flooring installation and replacement",
+        ],
+    },
+    {
+        image: "https://picsum.photos/200",
+        name: "Exterior Home Repair and Maintenance",
+        listItems: [
+            "Deck repair and staining",
+            "Gutter cleaning and repair",
+            "Fence repair and replacement",
+            "Siding repair and replacement",
+            "Pressure washing",
+            "Solar-powered exterior lighting installation",
+        ],
+    },
+];
