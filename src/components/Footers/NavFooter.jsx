7da9c9ca7531e0efd9a292f7d4f170cb4e29b562
Jesse Krensel <jdkrensel@gmail.com>
2023-04-28 18:52:17 -0400

    feat: active link highlighting style: add styles

--- src/components/Footers/NavFooter.jsx
+++ src/components/Footers/NavFooter.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import logo from "public/assets/logo.svg";
+import { useRouter } from "next/router";
 
 export default function NavFooter() {
+    // const [isActiveIndex, setIsActiveIndex] = useState()
+    const router = useRouter();
+    const path = router.pathname;
+    const pages = ["Home", "About", "Services", "Work", "Contact"];
+    const pagesListItems = pages.map((page, index) => {
+        const href = `/${page.toLowerCase()}`;
+        return (
+            <li
+                key={index}
+                data-active={
+                    href === path || (href === "/home" && path === "/")
+                }
+                className="p-1 text-center text-[#54575a] data-[active=true]:text-gray-400 tracking-wider sm:px-2 md:px-3"
+            >
+                <Link href={href !== "/home" ? href : "/"}>
+                    {page.toUpperCase()}
+                </Link>
+            </li>
+        );
+    });
     return (
-        <div className="bg-slate-500">
-            <div className="container mx-auto flex justify-around">
-                <Image src="" alt="Serenity Home Repair Logo" />
-                <ul>
-                    {["Home", "About", "Services", "Work", "Contact"].map(
-                        (page, index) => {
-                            return (
-                                <li key={index}>
-                                    <Link href={`/${page.toLowerCase()}`}>
-                                        {page}
-                                    </Link>
-                                </li>
-                            );
-                        }
-                    )}
-                </ul>
+        <section className="bg-slate-800 py-12 px-4">
+            <div className="container mx-auto flex justify-center gap-20">
+                <div className="relative h-36 w-36">
+                    <Image
+                        src={logo}
+                        alt="Serenity Home Repair Logo"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
+                <ul className="flex flex-col sm:flex-row sm:items-center">{pagesListItems}</ul>
             </div>
-        </div>
+        </section>
     );
 }
