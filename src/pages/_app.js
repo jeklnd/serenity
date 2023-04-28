cc20ad22bd5fe80a29672a639e680081f285a78a
Jesse Krensel <jdkrensel@gmail.com>
2023-04-28 19:01:57 -0400

    add vercel Analytics component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
     return (
         <React.StrictMode>
             <Component {...pageProps} />
+            <Analytics />
         </React.StrictMode>
     );
 }
