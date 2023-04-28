e87389e817ba9a3e94663376e6bcc9a80103967a
Jesse Krensel <jdkrensel@gmail.com>
2023-04-28 18:51:29 -0400

    style

--- src/components/Footers/CopyrightFooter.jsx
+++ src/components/Footers/CopyrightFooter.jsx
@@ -4,10 +4,11 @@ export default function CopyrightFooter() {
     const date = new Date();
     const year = date.getFullYear();
     return (
-        <div className="bg-slate-600">
-            <div className="container mx-auto text-center">
-                <p>© {year} Serenity Home Repair. All rights reserved.</p>
-                <p>Website designed and developed by Jesse Krensel.</p>
+        <div className="bg-slate-900 py-12">
+            <div className="container mx-auto text-center text-[#54575a] text-sm font-normal tracking-wide flex justify-center">
+                <p>
+                    © {year} Serenity Home Repair. All rights reserved. Site design and development by Jesse Krensel.
+                </p>
             </div>
         </div>
     );
