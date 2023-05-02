b8c57bd18dc098f009a895291ffd9d21e29dec1e
Jesse Krensel <jdkrensel@gmail.com>
2023-05-02 19:09:32 -0400

    add unique id to form with useId

--- src/components/RequestForm/RequestQuoteDropdown.jsx
+++ src/components/RequestForm/RequestQuoteDropdown.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useId } from "react";
 import RequestQuoteForm from "@/components/RequestForm/RequestQuoteForm.jsx";
 
 export default function RequestQuoteDropdown({ isDroppedDown }) {
+    const id = useId();
+
     return (
         <div
             data-open={isDroppedDown}
             className="-translate-y-full data-[open=true]:translate-y-0 transition flex justify-center items-start bg-gray-300 h-fit w-screen absolute z-0"
         >
             <div className="py-8 px-2 relative">
-                <RequestQuoteForm isInDropdown={true} />
+                <RequestQuoteForm isInDropdown={true} id={id} />
             </div>
         </div>
     );
