e6079ea8cb35845566f9eeee1ce2a74115953aea
Jesse Krensel <jdkrensel@gmail.com>
2023-05-02 19:12:49 -0400

    add styles

--- src/components/TestimonialSlider/CarouselArrows.jsx
+++ src/components/TestimonialSlider/CarouselArrows.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 
+const arrow_styles =
+    "text-gray-500 w-6 h-6 hover:text-gray-600 active:text-gray-700 transition";
+
 export function RightArrow({ increment }) {
     return (
         <button type="button" onClick={increment}>
-            <ArrowRightIcon className="text-gray-600/80 w-6 h-6 hover:text-gray-800 active:text-gray-800" />
+            <ArrowRightIcon className={arrow_styles} />
         </button>
     );
 }
@@ -12,7 +15,7 @@ export function RightArrow({ increment }) {
 export function LeftArrow({ decrement }) {
     return (
         <button type="button" onClick={decrement}>
-            <ArrowLeftIcon className="text-gray-600/80 w-6 h-6 hover:text-gray-800 active:text-gray-800" />
+            <ArrowLeftIcon className={arrow_styles} />
         </button>
     );
 }
