84c619c4b34531983bf7aca093c446283a482862
Jesse Krensel <jdkrensel@gmail.com>
2023-05-02 19:09:04 -0400

    add oneOffClasses and onClick props

--- src/components/RequestForm/RequestQuoteBtn.jsx
+++ src/components/RequestForm/RequestQuoteBtn.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function RequestQuoteBtn({ text, onToggleModal }) {
+export default function RequestQuoteBtn({ text, oneOffClasses = "", onClick }) {
     return (
         <button
-            type="button"
-            className="font-extrabold text-gray-800 bg-[#ffc52f]/90 px-4 py-2 rounded-sm text-sm"
-            onClick={onToggleModal}
+            type="submit"
+            className={`${oneOffClasses} w-fit font-extrabold text-gray-800 bg-[#ffc52f]/90 px-4 py-2 my-2 rounded-sm text-sm border-t-2 border-sy-400/50 shadow-sm hover:shadow-md active:shadow-none`}
+            onClick={onClick}
         >
             {text}
         </button>
