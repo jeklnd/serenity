b0a42b33dece46e4863128f6d52c7f64f7e18c91
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:55:08 -0400

    add hover/active state styles

--- src/components/TestimonialSlider/CarouselArrows.jsx
+++ src/components/TestimonialSlider/CarouselArrows.jsx
@@ -4,7 +4,7 @@ import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 export function RightArrow({ increment }) {
     return (
         <button type="button" onClick={increment}>
-            <ArrowRightIcon className={`text-gray-600/80 w-6 h-6`} />
+            <ArrowRightIcon className="text-gray-600/80 w-6 h-6 hover:text-gray-800 active:text-gray-800" />
         </button>
     );
 }
@@ -12,7 +12,7 @@ export function RightArrow({ increment }) {
 export function LeftArrow({ decrement }) {
     return (
         <button type="button" onClick={decrement}>
-            <ArrowLeftIcon className={`text-gray-600/80 w-6 h-6`} />
+            <ArrowLeftIcon className="text-gray-600/80 w-6 h-6 hover:text-gray-800 active:text-gray-800" />
         </button>
     );
 }
