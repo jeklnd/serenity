08e07e5fde36668cf5e57a642567a13164785711
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:44:50 -0400

    add component and style

---
+++ src/components/Map/ProudlyServing.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function ProudlyServing() {
+    return (
+        <div className=" bg-slate-400 pt-12 pb-1 flex flex-col gap-9">
+            <div className="container mx-auto text-center">
+                <h2 className="text-3xl font-bold text-gray-700 mb-2">
+                    Proudly serving
+                </h2>
+                <p className="text-xl text-gray-600">
+                    Serenity Home Repair caters to all Montgomery and
+                    Philadelphia County postal codes.
+                </p>
+            </div>
+            <div className="flex justify-center bg-slate-300 w-full">
+                <iframe
+                    width="600"
+                    height="450"
+                    className="border-0 w-full"
+                    loading="lazy"
+                    allowFullScreen
+                    src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJfad1ODq8xokRFap-dkA7Z7k&key=${process.env.GOOGLE_MAPS_EMBED_API_KEY}`}
+                ></iframe>
+            </div>
+        </div>
+    );
+}
