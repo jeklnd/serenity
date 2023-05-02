4c114f9230f16caec8f5adba812eb9e1fc20a3c2
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:43:54 -0400

    add policies and consent preferences

--- src/components/Footers/CopyrightFooter.jsx
+++ src/components/Footers/CopyrightFooter.jsx
@@ -1,14 +1,50 @@
 import React from "react";
+import Link from "next/link";
 
 export default function CopyrightFooter() {
     const date = new Date();
     const year = date.getFullYear();
     return (
         <div className="bg-slate-900 py-12">
-            <div className="container mx-auto text-center text-[#54575a] text-sm font-normal tracking-wide flex justify-center">
-                <p>
-                    © {year} Serenity Home Repair. All rights reserved. Site design and development by Jesse Krensel.
-                </p>
+            <div className="container mx-auto text-center text-[#54575a] text-sm font-normal tracking-wide flex flex-col justify-center">
+                <p>© {year} Serenity Home Repair. All rights reserved.</p>
+                <br />
+                <div>
+                    <p>
+                        {" "}
+                        <Link
+                            href="/privacy-policy"
+                            className="hover:text-gray-500"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Privacy Policy
+                        </Link>
+                        <span> | </span>
+                        <Link
+                            href="/cookie-policy"
+                            className="hover:text-gray-500"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Cookie Policy
+                        </Link>
+                        <span> | </span>
+                        <Link
+                            href="#"
+                            className="hover:text-gray-500"
+                            onClick={() => {
+                                window.displayPreferenceModal();
+                                return false;
+                            }}
+                            id="termly-consent-preferences"
+                        >
+                            Consent Preferences
+                        </Link>
+                    </p>
+                </div>
+                {/* <br /> */}
+                {/* <p>Design and development by Jesse Krensel</p> */}
             </div>
         </div>
     );
