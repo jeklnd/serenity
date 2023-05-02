124ce40ae068fbc0ed87c4236094bc0dd97bf139
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:55:58 -0400

    initial add

---
+++ src/components/TestimonialSlider/TestimonialHeading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Heading() {
+    return (
+        <h2 className="text-center font-bold text-3xl text-gray-700">
+            Our clients love us.
+        </h2>
+    );
+}
