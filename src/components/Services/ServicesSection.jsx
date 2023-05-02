0fc4445b6b0d2ba4b39e318a0d8e594441e84496
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 22:35:17 -0400

    add experimental list item headers

--- src/components/Services/ServicesSection.jsx
+++ src/components/Services/ServicesSection.jsx
@@ -11,32 +11,35 @@ export default function ServiceSection() {
 
     const roomsListItems = rooms.map((room, index) => {
         return (
-            <li
-                key={index}
-                className="flex items-stretch relative aspect-square group cursor-pointer"
-            >
+            <li key={index} className="relative group">
+                {/* <div className="z-10 text-3xl font-extrabold tracking-wide text-gray-700 relative text-center bg-gray-200 group-hover:hidden">
+                    <h1 className="inline  ">{room.room}</h1>
+                </div> */}
+
                 <Image
                     src={room.image}
                     alt={`Image of ${room.room}`}
                     fill
                     className="absolute object-cover rounded-sm group-hover:brightness-150 group-hover:opacity-25 transition"
                 />
-                <div className="z-10 p-8 opacity-0 group-hover:opacity-100 flex flex-col justify-around">
-                    <h1 className="text-3xl font-bold text-gray-900">
-                        {room.room}
-                    </h1>
-                    <p className="text-lg font-normal text-gray-900">
-                        {room.text}
-                    </p>
-                    <button
-                        type="button"
-                        className="text-left font-medium text-md text-gray-700 border-2 bg-gray-200/25 border-gray-700 mx-auto px-2 py-1 hover:shadow-sm hover:bg-gray-200/85 hover:text-gray-800 hover:border-gray-800 transition rounded-sm"
-                    >
-                        <Link href="/services">
-                            View Services{" "}
-                            <ArrowSmallRightIcon className="inline h-5 w-5" />
-                        </Link>
-                    </button>
+                <div className="flex items-stretch relative aspect-square cursor-pointer">
+                    <div className="z-10 p-8 opacity-0 group-hover:opacity-100 flex flex-col justify-around">
+                        <h1 className="text-3xl font-bold text-gray-900">
+                            {room.room}
+                        </h1>
+                        <p className="text-lg font-normal text-gray-900">
+                            {room.text}
+                        </p>
+                        <button
+                            type="button"
+                            className="text-left font-medium text-md text-gray-700 border-2 bg-gray-200/25 border-gray-700 mx-auto px-2 py-1 hover:shadow-sm hover:bg-gray-200/85 hover:text-gray-800 hover:border-gray-800 transition rounded-sm"
+                        >
+                            <Link href="/services">
+                                View Services{" "}
+                                <ArrowSmallRightIcon className="inline h-5 w-5" />
+                            </Link>
+                        </button>
+                    </div>
                 </div>
             </li>
         );
@@ -68,7 +71,7 @@ export default function ServiceSection() {
                 </ul>
 
                 {/* By Service*/}
-                                <ul
+                <ul
                     data-selected={selectedSide}
                     className="hidden data-[selected='right']:grid grid-cols-1 max-w-[400px] sm:grid-cols-2 sm:max-w-[980px] gap-6"
                 >
