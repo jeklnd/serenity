0302cac0378e56980b34f5a7b72708a5a56aea36
Jesse Krensel <jdkrensel@gmail.com>
2023-05-02 19:03:10 -0400

    style

--- src/components/Contact/AccentBorder.jsx
+++ src/components/Contact/AccentBorder.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function AccentBorder() {
-    return <div className="absolute w-16 border-2 border-sy-500 top-8"></div>;
+    return <div className="w-16 border-2 border-sy-500"></div>;
 }
