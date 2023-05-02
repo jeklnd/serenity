a537a75953a91f3473b736c5c87084e109b445da
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:52:07 -0400

    rename, add styles

--- src/components/Services/ServicesBullets.jsx
+++ src/components/Services/ServicesBullets.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import { rooms, services } from "./data.js";
+import { services } from "./data.js";
 
 export default function ServicesCards() {
     const servicesListItems = services.map((service, index) => {
         return (
-            <li key={index} className="flex items-stretch py-2">
+            <li key={index} className="flex flex-col py-6 gap-2 rounded-sm shadow-md">
+                {/* Images */}
                 {/* <div
                     data-parity={index % 2 !== 0 && "odd"}
                     className="relative w-36 h-36 data-[parity='odd']:order-last"
@@ -17,19 +18,22 @@ export default function ServicesCards() {
                         className="object-contain"
                     ></Image>
                 </div> */}
-                <div>
-                    <h2 className="text-xl font-medium text-gray-700">
+                <div className="mx-auto text-3xl text-sy-600">
+                    {service.icon}
+                </div>
+                <div className="p-4">
+                    <h2 className="text-xl font-medium text-gray-700 text-center">
                         {service.name}
                     </h2>
                     <ul>
-                        {service.listItems.map((s, i) => {
+                        {service.listItems.map((listItem, i) => {
                             return (
                                 <li
                                     key={i}
                                     className="list-disc list-inside marker:text-sy-600 pl-6"
                                 >
                                     <p className="inline text-lg text-gray-600">
-                                        {s}
+                                        {listItem}
                                     </p>
                                 </li>
                             );
@@ -41,8 +45,10 @@ export default function ServicesCards() {
     });
 
     return (
-        <div className="container mx-auto flex justify-center">
-            <ul className="p-4">{servicesListItems}</ul>
+        <div className="container mx-auto flex justify-center py-8">
+            <ul className="p-4 grid md:grid-cols-2 justify-center flex-wrap gap-8">
+                {servicesListItems}
+            </ul>
         </div>
     );
 }
