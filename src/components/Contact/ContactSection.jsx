d460218468d9d2aa4328cc4cc078f7ed9d9d0784
Jesse Krensel <jdkrensel@gmail.com>
2023-05-02 19:05:03 -0400

    add unique form id with useId add style

--- src/components/Contact/ContactSection.jsx
+++ src/components/Contact/ContactSection.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useId} from "react";
 import RequestQuoteForm from "src/components/RequestForm/RequestQuoteForm.jsx";
 
 export default function ContactSection() {
+    const id = useId();
     return (
-        <div className="bg-slate-300 py-8">
-            {" "}
-            <h1 className="text-3xl font-bold text-gray-700 text-center py-8">
-                Contact Us
-            </h1>
-            <RequestQuoteForm />
+        <div className="bg-slate-400 py-8">
+            <div className="bg-slate-300 w-fit mx-auto py-6 rounded-sm shadow-md">
+                <h1 className="text-3xl font-bold text-gray-700 text-center p-4">
+                    Contact Us
+                </h1>
+                <RequestQuoteForm id={id} />
+            </div>
         </div>
     );
 }
