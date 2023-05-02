369ba58636073840ba6fdefbbe36825e37cf2858
Jesse Krensel <jdkrensel@gmail.com>
2023-05-02 19:13:25 -0400

    style: add padding

--- src/components/TestimonialSlider/TestimonialHeading.jsx
+++ src/components/TestimonialSlider/TestimonialHeading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Heading() {
     return (
-        <h2 className="text-center font-bold text-3xl text-gray-700">
+        <h2 className="text-center font-bold text-3xl text-gray-700 pb-4">
             Our clients love us.
         </h2>
     );
