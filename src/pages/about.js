e43eada7bd453244b8948b165a3ae3453c5dc77b
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:57:58 -0400

    add image, copy, and styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar.jsx";
-import RequestQuoteForm from "@/components/RequestForm/RequestQuoteForm.jsx";
+import Image from "next/image";
+import ContactSection from "@/components/Contact/ContactSection.jsx";
 import NavFooter from "@/components/Footers/NavFooter.jsx";
 import CopyrightFooter from "@/components/Footers/CopyrightFooter.jsx";
+import aboutUsImage from "public/assets/about-us.jpg";
 
 export default function about() {
     return (
@@ -10,13 +12,38 @@ export default function about() {
             <header className="sticky top-0 z-40">
                 <NavBar />
             </header>
-            <main className="grow flex flex-col">
-                <h1>
-                    What is Serenity Home Repair? What is the mission of
-                    Serenity Home Repair? What is the background of serenity
-                    Home repair? Why should I hire serenity home repair?
+            <main className="grow flex flex-col items-center bg-gray-300">
+                <h1 className="text-3xl font-bold text-gray-700 text-center py-8">
+                    About
                 </h1>
-                <RequestQuoteForm />
+                <div className="container gap-4 flex flex-col md:flex-row p-2">
+                    <Image
+                        src={aboutUsImage}
+                        alt="image of Robert"
+                        className="rounded-sm object-contain max-h-72 sm:max-h-96"
+                    ></Image>
+
+                    <p className="text-gray-600 text-md">
+                        Serenity Home Repair is your go-to local handyman for
+                        all your home repair and maintenance needs. With a wide
+                        range of services, including carpentry, plumbing,
+                        electrical, and exterior home repair, our team of
+                        experts is equipped to handle any project you have in
+                        mind. Whether you need a simple repair or a full kitchen
+                        or bathroom remodel, our skilled professionals have the
+                        experience and knowledge to get the job done efficiently
+                        and effectively. <br />
+                        <br />
+                        We take pride in providing exceptional customer service
+                        and delivering high-quality workmanship that exceeds
+                        your expectations. Contact Serenity Home Repair today to
+                        schedule a consultation and let us take care of all your
+                        home repair needs.
+                    </p>
+                </div>
+                <div className="w-full">
+                    <ContactSection />
+                </div>
             </main>
             <footer>
                 <NavFooter />
