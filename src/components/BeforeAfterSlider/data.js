ad10c13954a23705072cf125d29321fe26615beb
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 20:25:45 -0400

    add before/after data

---
+++ src/components/BeforeAfterSlider/data.js
@@ -0,0 +1,35 @@
+import stepsBefore from "public/assets/beforeAndAfterImages/Scott_front-steps_before-scaled-puiwtl5x485xvvnuvcawkc94sqrojuowugszv3n894.jpg";
+import stepsAfter from "public/assets/beforeAndAfterImages/Scott_front-steps_after-scaled-puiwtdn7ljvnazys391w0e5g1nsqu9v25fl40vydmw.jpg";
+import sillBefore from "public/assets/beforeAndAfterImages/rotting-window-sill_before_2-scaled-puiwrphjg5lanye5uj1rktby10195n88l5u1b6fkp4.jpg";
+import sillAfter from "public/assets/beforeAndAfterImages/Rotting-wall-and-window-sill_after-rotated-puiwrbdyln1ztsyn4uyd1ew147yqy6o9j81r410hag.jpg";
+import doorBefore from "public/assets/beforeAndAfterImages/Scott_screen-door-rebuild_before-scaled-puiwt1fb4pex42gj2lrqlz8gbngz27ijrr3ssaghvs.jpg";
+import doorAfter from "public/assets/beforeAndAfterImages/Scott_screen-door-rebuild_after-puiwpzg6x38ddow9ws8e08zitkj11mdycmrzmwzk3s.jpg";
+import houseBefore from "public/assets/beforeAndAfterImages/Rear-of-house_before-rotated-puiwq53823g3bco2zuo5f7kadvr8bt0cdeowikr72g.jpg";
+import houseAfter from "public/assets/beforeAndAfterImages/Rear-of-house_after-scaled-puiws985focbfrlhn9kxj6cmi3bynaelnvj8dzmb2g.jpg";
+
+export const couples = [
+    {
+        before: stepsBefore,
+        after: stepsAfter,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor nunc, semper sit amet euismod ultricies, venenatis et libero. ",
+    },
+    {
+        before: sillBefore,
+        after: sillAfter,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor nunc, semper sit amet euismod ultricies, venenatis et libero. ",
+    },
+    {
+        before: doorBefore,
+        after: doorAfter,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor nunc, semper sit amet euismod ultricies, venenatis et libero. ",
+    },
+    {
+        before: houseBefore,
+        after: houseAfter,
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dolor nunc, semper sit amet euismod ultricies, venenatis et libero. ",
+    },
+];
