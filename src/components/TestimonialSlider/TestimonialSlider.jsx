93efe8f4009964275d744268e41ce29201797f23
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:56:27 -0400

    add setInterval effect, extract components

--- src/components/TestimonialSlider/TestimonialSlider.jsx
+++ src/components/TestimonialSlider/TestimonialSlider.jsx
@@ -1,25 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
-import logo from "public/assets/logo.svg";
 import { testimonials } from "./data.js";
-import Heading from "./Heading.jsx";
+import Heading from "./TestimonialHeading.jsx";
 import { LeftArrow, RightArrow } from "./CarouselArrows.jsx";
+import TestimonialBio from "./TestimonialBio.jsx";
 import CarouselDots from "./CarouselDots.jsx";
 import QuotationMark from "./QuotationMark.jsx";
 
 export default function TestimonialSlider() {
     const [isVisibleIndex, setIsVisibleIndex] = useState(0);
-    const increment = () => {
+
+    const increment = useCallback(() => {
         isVisibleIndex < testimonials.length - 1
             ? setIsVisibleIndex(isVisibleIndex + 1)
             : setIsVisibleIndex(0);
-    };
-    const decrement = () => {
+    }, [isVisibleIndex]);
+
+    const decrement = useCallback(() => {
         isVisibleIndex > 0
             ? setIsVisibleIndex(isVisibleIndex - 1)
             : setIsVisibleIndex(testimonials.length - 1);
-    };
+    }, [isVisibleIndex]);
+
     const [xPosition, setXPosition] = useState(null);
+
+    useEffect(() => {
+        const interval = setInterval(increment, 12000);
+        return () => clearInterval(interval);
+    }, [isVisibleIndex, increment]);
 
     return (
         <section
@@ -27,19 +35,19 @@ export default function TestimonialSlider() {
             className="bg-sy-400/80 px-4 py-14 min-h-[42rem] sm:min-h-[24rem]"
         >
             <Heading />
-            <div className="container mx-auto flex justify-center items-stretch text-center max-w-xl min-h-[32rem] sm:min-h-[24rem]">
+            <div className="container mx-auto flex justify-center items-stretch text-center max-w-xl sm:min-h-[20rem]">
                 <LeftArrow decrement={decrement} />
 
                 <ul
-                    className="flex py-12 px-2 sm:px-6 items-center"
+                    className="flex py-8 sm:py-8 px-2 sm:px-6 items-center"
                     onTouchStart={(e) => {
                         setXPosition(e.changedTouches[0].clientX);
                     }}
                     onTouchEnd={(e) => {
-                        if (e.changedTouches[0].clientX < xPosition) {
-                            increment();
-                        } else {
+                        if (e.changedTouches[0].clientX > xPosition) {
                             decrement();
+                        } else {
+                            increment();
                         }
                     }}
                 >
@@ -48,31 +56,19 @@ export default function TestimonialSlider() {
                             <li
                                 key={index}
                                 data-visible={index === isVisibleIndex}
-                                className="hidden data-[visible=true]:flex data-[visible=false]:opacity-0 flex-col gap-12 text-gray-800 tracking-wider leading-6 text-lg font-light transition"
+                                className="hidden data-[visible=true]:flex data-[visible=false]:opacity-0 flex-col gap-8 sm:gap-12 text-gray-800 tracking-wider leading-6 text-lg font-light transition"
                             >
-                                <div className="flex [&>*:last-child]:self-end gap-2">
+                                <div className="flex [&>*:last-child]:self-end">
                                     <QuotationMark />
-                                    <p className="text-md">
+                                    <p className="text-sm font-normal text-gray-600 sm:text-md">
                                         {testimonial.text}
                                     </p>
                                     <QuotationMark />
                                 </div>
-
-                                <div className="flex justify-center gap-4">
-                                    <Image
-                                        src={logo}
-                                        alt="Serenity Home Repair Logo"
-                                        className="h-12 w-12"
-                                    ></Image>
-                                    <div>
-                                        <p className="font-bold text-gray-600">
-                                            {testimonial.name}
-                                        </p>
-                                        <p className="text-sm font-normal text-gray-600">
-                                            {testimonial.location}
-                                        </p>
-                                    </div>
-                                </div>
+                                <TestimonialBio
+                                    name={testimonial.name}
+                                    location={testimonial.location}
+                                />
                             </li>
                         );
                     })}
