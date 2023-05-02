30a1f5af12817f0a5a4afce3cd99c26e239898e7
Jesse Krensel <jdkrensel@gmail.com>
2023-05-02 19:12:14 -0400

    add bricks heading and styles

--- src/components/Services/ServicesBullets.jsx
+++ src/components/Services/ServicesBullets.jsx
@@ -1,36 +1,31 @@
 import React from "react";
-import Image from "next/image";
 import { services } from "./data.js";
+import styles from "./ServiceBullets.module.css";
 
 export default function ServicesCards() {
     const servicesListItems = services.map((service, index) => {
         return (
-            <li key={index} className="flex flex-col py-6 gap-2 rounded-sm shadow-md">
+            <li
+                key={index}
+            >
                 {/* Images */}
-                {/* <div
-                    data-parity={index % 2 !== 0 && "odd"}
-                    className="relative w-36 h-36 data-[parity='odd']:order-last"
-                >
-                    <Image
-                        src={service.image}
-                        alt={`${service.name} image`}
-                        fill
-                        className="object-contain"
-                    ></Image>
-                </div> */}
-                <div className="mx-auto text-3xl text-sy-600">
-                    {service.icon}
-                </div>
-                <div className="p-4">
-                    <h2 className="text-xl font-medium text-gray-700 text-center">
-                        {service.name}
-                    </h2>
-                    <ul>
+                <div className="m-2 flex flex-col gap-2 rounded-sm shadow-md">
+                    <div className={`${styles.bricks} flex flex-col p-4 pb-6 gap-2 relative rounded-sm`}>
+                        <div className="mx-auto text-3xl text-gray-700">
+                            {service.icon}
+                        </div>
+                        <h2 className="text-xl font-medium text-gray-700 text-center">
+                            {service.name}
+                        </h2>
+
+                        <div className="w-12 border-2 border-gray-700/75 self-center absolute bottom-0"></div>
+                    </div>
+                    <ul className="p-2">
                         {service.listItems.map((listItem, i) => {
                             return (
                                 <li
                                     key={i}
-                                    className="list-disc list-inside marker:text-sy-600 pl-6"
+                                    className="list-disc list-inside marker:text-sy-600 pl-2"
                                 >
                                     <p className="inline text-lg text-gray-600">
                                         {listItem}
@@ -46,7 +41,7 @@ export default function ServicesCards() {
 
     return (
         <div className="container mx-auto flex justify-center py-8">
-            <ul className="p-4 grid md:grid-cols-2 justify-center flex-wrap gap-8">
+            <ul className=" grid md:grid-cols-2 justify-center flex-wrap gap-8">
                 {servicesListItems}
             </ul>
         </div>
