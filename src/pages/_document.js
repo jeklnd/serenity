10458733c9215f28fbac44c2c267764d60eea52b
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:57:42 -0400

    feat: add cookie consent banner

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <script
+                    type="text/javascript"
+                    src="https://app.termly.io/embed.min.js"
+                    data-auto-block="on"
+                    data-website-uuid="13388011-6506-4da0-b438-9aa79e263552"
+                    defer
+                />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
