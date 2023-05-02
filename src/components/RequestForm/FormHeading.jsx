e66e0e51a3ea4134e57b4acbb10a3bcca06aadc9
Jesse Krensel <jdkrensel@gmail.com>
2023-05-01 21:45:31 -0400

    style: adjust font size

--- src/components/RequestForm/FormHeading.jsx
+++ src/components/RequestForm/FormHeading.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function FormHeading() {
     return (
         <div className="flex flex-col text-center">
-            <p className="text-lg font-semibold text-gray-700 pb-0.5">
+            <p className="text-xl font-semibold text-gray-700 pb-0.5">
                 Request a complimentary quote
             </p>
-            <p className="text-xs text-gray-400 pb-2">
+            <p className="text-sm text-gray-400 pb-2">
                 Submit your request and Serenity Home Repair will contact you
                 via phone or email in 24-48 hours.
             </p>
