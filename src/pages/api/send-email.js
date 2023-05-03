4bafdd1f05725d3e69d711328aa4b6cd07570f2a
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 12:44:07 -0400

    update redirect to "/success" page

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
             console.log("Sent message");
             // res.status(200).json({message: "Email sent successfully."})
             // res.status(200).send("Email sent successfully")
-            res.redirect(303, "/");
+            res.redirect(303, "/success");
         })
         .catch((error) => {
             console.error(error);
