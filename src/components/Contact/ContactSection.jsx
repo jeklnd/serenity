a0eeaeb167d721c8b234bddb84d2a24af557df8a
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 19:57:59 -0400

    style

--- src/components/Contact/ContactSection.jsx
+++ src/components/Contact/ContactSection.jsx
@@ -1,16 +1,17 @@
-import React, {useId} from "react";
-import RequestQuoteForm from "src/components/RequestForm/RequestQuoteForm.jsx";
+import React from "react";
+import ContactInformation from "./ContactInformation.jsx";
+import ContactSectionForm from "./ContactSectionForm.jsx";
 
 export default function ContactSection() {
-    const id = useId();
     return (
-        <div className="bg-slate-400 py-8">
-            <div className="bg-slate-300 w-fit mx-auto py-6 rounded-sm shadow-md">
-                <h1 className="text-3xl font-bold text-gray-700 text-center p-4">
-                    Contact Us
-                </h1>
-                <RequestQuoteForm id={id} />
+        <section className=" bg-slate-700 ">
+            <div className="bg-slate-400 w-screen flex justify-center relative overflow-hidden lg:py-16">
+                <div className="w-full relative flex flex-col gap-0 md:flex-row justify-center items-center z-10 p-2">
+                    <ContactInformation classesFromParent="pt-12 pb-6 sm:px-12 sm:py-12 w-fit lg:bg-slate-200 rounded-md text-gray-700 lg:text-gray-700 lg:px-14 lg:py-8 lg:shadow-md" />
+                    <ContactSectionForm classesFromParent="py-6 lg:py-8 w-fit shadow:sm rounded-md lg:relative lg:-mb-40 lg:-ml-8 bg-slate-300 lg:shadow-lg" heading="Request Quote"/>
+                </div>
+                <div className="absolute bottom-0 w-[300vw] h-1/2 lg:h-full bg-slate-800 -rotate-[30deg] lg:-rotate-[15deg] translate-y-72 sm:max-lg:translate-x-12"></div>
             </div>
-        </div>
+        </section>
     );
 }
