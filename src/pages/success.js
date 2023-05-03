7b1d847bd9a4ba04e014c6eaf6eb414001b2db9c
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 12:43:55 -0400

    initial add

---
+++ src/pages/success.js
@@ -0,0 +1,32 @@
+import NavBar from "@/components/NavBar/NavBar.jsx";
+import SuccessMessage from "@/components/RequestForm/SuccessMessage.jsx";
+import HeroSection from "@/components/HeroSection/HeroSection.jsx";
+import TestimonialSlider from "@/components/TestimonialSlider/TestimonialSlider.jsx";
+import ServicesSection from "@/components/Services/ServicesSection.jsx";
+import ProudlyServing from "@/components/Map/ProudlyServing.jsx";
+import ContactSection from "@/components/Contact/ContactSection.jsx";
+import NavFooter from "@/components/Footers/NavFooter.jsx";
+import CopyrightFooter from "@/components/Footers/CopyrightFooter.jsx";
+
+
+export default function Home() {
+    return (
+        <>
+            <header className="sticky top-0 z-40">
+                <NavBar />
+                <SuccessMessage />
+            </header>
+            <main className="grow flex flex-col justify-around">
+                <HeroSection />
+                <TestimonialSlider />
+                <ServicesSection />
+                <ProudlyServing />
+                <ContactSection />
+            </main>
+            <footer>
+                <NavFooter />
+                <CopyrightFooter />
+            </footer>
+        </>
+    );
+}
