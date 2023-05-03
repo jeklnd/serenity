88b0526a3841a540156f9db891e6b8f18263270c
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 19:59:07 -0400

    style text and spacing add decorative div

--- src/components/Map/ProudlyServing.jsx
+++ src/components/Map/ProudlyServing.jsx
@@ -2,21 +2,23 @@ import React from "react";
 
 export default function ProudlyServing() {
     return (
-        <div className=" bg-slate-400 pt-12 pb-1 flex flex-col gap-9">
-            <div className="container mx-auto text-center">
-                <h2 className="text-3xl font-bold text-gray-700 mb-2">
+        <div className=" bg-slate-500 pt-16 flex flex-col relative">
+            <div className="container mx-auto text-center px-2">
+                <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-800 mb-2">
                     Proudly serving
                 </h2>
-                <p className="text-xl text-gray-600">
+                <p className="text-xl lg:text-2xl text-gray-700">
                     Serenity Home Repair caters to all Montgomery and
                     Philadelphia County postal codes.
                 </p>
             </div>
-            <div className="flex justify-center bg-slate-300 w-full">
+            <div className="flex justify-center bg-transparent w-full relative">
+                <div className="lg:hidden bg-slate-700 w-screen h-1/2 absolute z-10 bottom-0"></div>
+
                 <iframe
                     width="600"
                     height="450"
-                    className="border-0 w-full"
+                    className="border-0 w-full z-10 my-10 lg:mb-0 shadow-md lg:h-[600px]"
                     loading="lazy"
                     allowFullScreen
                     src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJfad1ODq8xokRFap-dkA7Z7k&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
