6b9707a4078dc56932ec100d34cb19493e56031d
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 19:57:22 -0400

    add classes prop

--- src/components/Contact/AccentBorder.jsx
+++ src/components/Contact/AccentBorder.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function AccentBorder() {
-    return <div className="w-16 border-2 border-sy-500"></div>;
+export default function AccentBorder({ classes = "" }) {
+    return <div className={` w-16 border-2 border-sy-500 ${classes}`}></div>;
 }
