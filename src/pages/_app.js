8eae158b0139a7215d5122909ad0adec52542733
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 10:39:45 -0400

    remove strict mode from production deployment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,9 +4,11 @@ import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
     return (
-        <React.StrictMode>
+        <>
+            {/* <React.StrictMode> */}
             <Component {...pageProps} />
             <Analytics />
-        </React.StrictMode>
+            {/* </React.StrictMode> */}
+        </>
     );
 }
