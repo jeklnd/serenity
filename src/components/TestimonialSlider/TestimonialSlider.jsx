335c4ee563a44de2fbc187858073a1ff92bcaadc
Jesse Krensel <jdkrensel@gmail.com>
2023-05-04 14:26:56 -0400

    style: debug section height change on testimonial increment

--- src/components/TestimonialSlider/TestimonialSlider.jsx
+++ src/components/TestimonialSlider/TestimonialSlider.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { testimonials } from "./data.js";
-import Heading from "./TestimonialHeading.jsx";
 import { LeftArrow, RightArrow } from "./CarouselArrows.jsx";
 import TestimonialBio from "./TestimonialBio.jsx";
 import CarouselDots from "./CarouselDots.jsx";
@@ -29,56 +28,71 @@ export default function TestimonialSlider() {
         return () => clearInterval(interval);
     }, [isVisibleIndex, increment]);
 
+    const handleTouchStart = (e) => {
+        setXPosition(e.changedTouches[0].clientX);
+    };
+
+    const handleTouchEnd = (e) => {
+        if (e.changedTouches[0].clientX > xPosition) {
+            decrement();
+        } else {
+            increment();
+        }
+    };
+
     return (
         <section
             id="testimonials"
-            className={`px-4 py-14 min-h-[42rem] sm:min-h-[24rem] ${styles.bg}`}
+            className={`${styles.bg} flex flex-col justify-center`}
         >
-            <Heading />
-            <div className="container mx-auto flex justify-center items-stretch text-center max-w-xl sm:min-h-[20rem]">
-                <LeftArrow decrement={decrement} />
+            <h2
+                className={`mt-12 mb-8 text-center font-bold text-3xl sm:text-4xl lg:text-5xl text-gray-700`}
+            >
+                Our clients love us.
+            </h2>
+            <div className={`pb-10 px-2  gap-4 flex flex-col justify-center`}>
+                <div className="container mx-auto flex justify-center items-stretch text-center max-w-xl">
+                    <LeftArrow decrement={decrement} />
+                    <ul
+                        className="flex"
+                        onTouchStart={handleTouchStart}
+                        onTouchEnd={handleTouchEnd}
+                    >
+                        <QuotationMark />
+                        {testimonials.map((testimonial, index) => {
+                            return (
+                                <li key={index}>
+                                    <div
+                                        data-visible={index === isVisibleIndex}
+                                        className="hidden data-[visible=true]:flex flex-col max-[375px]:min-h-[600px] max-[426px]:min-h-[560px] min-h-[450px] gap-8 sm:gap-12 text-gray-800 tracking-wider leading-6 text-lg font-light transition"
+                                    >
+                                        <div className="grow flex items-center">
+                                            <p className="text-sm  min-[425px]:text-lg lg:text-xl font-normal text-gray-600 italic leading-6">
+                                                {testimonial.text}
+                                            </p>
+                                        </div>
 
-                <ul
-                    className="flex py-8 sm:py-8 px-2 sm:px-6 items-center"
-                    onTouchStart={(e) => {
-                        setXPosition(e.changedTouches[0].clientX);
-                    }}
-                    onTouchEnd={(e) => {
-                        if (e.changedTouches[0].clientX > xPosition) {
-                            decrement();
-                        } else {
-                            increment();
-                        }
-                    }}
-                >
-                    {testimonials.map((testimonial, index) => {
-                        return (
-                            <li
-                                key={index}
-                                data-visible={index === isVisibleIndex}
-                                className="hidden data-[visible=true]:flex data-[visible=false]:opacity-0 flex-col gap-8 sm:gap-12 text-gray-800 tracking-wider leading-6 text-lg font-light transition"
-                            >
-                                <div className="flex [&>*:last-child]:self-end">
-                                    <QuotationMark />
-                                    <p className="text-sm min-[425px]:text-lg lg:text-xl font-normal text-gray-600  italic">
-                                        {testimonial.text}
-                                    </p>
-                                    <QuotationMark />
-                                </div>
-                                <TestimonialBio
-                                    name={testimonial.name}
-                                    location={testimonial.location}
-                                />
-                            </li>
-                        );
-                    })}
-                </ul>
-                <RightArrow increment={increment} />
+                                        <div className="flex flex-col gap-6">
+                                            <TestimonialBio
+                                                name={testimonial.name}
+                                                location={testimonial.location}
+                                            />
+                                            <CarouselDots
+                                                isVisibleIndex={isVisibleIndex}
+                                                onViewTestimonial={
+                                                    setIsVisibleIndex
+                                                }
+                                            />
+                                        </div>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                        <QuotationMark moreClasses="self-end pb-24" />
+                    </ul>
+                    <RightArrow increment={increment} />
+                </div>
             </div>
-            <CarouselDots
-                isVisibleIndex={isVisibleIndex}
-                onViewTestimonial={setIsVisibleIndex}
-            />
         </section>
     );
 }
