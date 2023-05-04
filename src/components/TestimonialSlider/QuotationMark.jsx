a788befe8115a3f99520597f2da996f9e98f634b
Jesse Krensel <jdkrensel@gmail.com>
2023-05-04 14:25:38 -0400

    add moreClasses prop

--- src/components/TestimonialSlider/QuotationMark.jsx
+++ src/components/TestimonialSlider/QuotationMark.jsx
@@ -5,9 +5,11 @@ const spaceGrotesque = localFont({
     src: "SpaceGrotesk.ttf",
 });
 
-export default function QuotationMark() {
+export default function QuotationMark({moreClasses}) {
     return (
-        <span className={`text-6xl leading-6 text-gray-600/40 ${spaceGrotesque.className} first:-ml-4 first:-my-2 last:-mr-4 last:-my-8`}>
+        <span
+            className={`text-6xl leading-6 text-gray-600/40 ${spaceGrotesque.className} ${moreClasses}`}
+        >
             &quot;
         </span>
     );
