4db1cd4493343e93a51961627538e123991a9c5b
Jesse Krensel <jdkrensel@gmail.com>
2023-05-04 14:25:42 -0400

    remove

--- src/components/TestimonialSlider/TestimonialHeading.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Heading() {
-    return (
-        <h2 className="text-center font-bold text-3xl sm:text-4xl lg:text-5xl text-gray-700 pb-4">
-            Our clients love us.
-        </h2>
-    );
-}
