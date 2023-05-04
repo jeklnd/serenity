4e3e476d377575f20bf691fbe962b3bb6991fb54
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 20:00:53 -0400

    darken backdrop

--- src/components/RequestForm/Backdrop.jsx
+++ src/components/RequestForm/Backdrop.jsx
@@ -4,7 +4,7 @@ export default function Backdrop({ isVisible, onClick }) {
     return (
         <div
             data-visible={isVisible}
-            className="invisible data-[visible=true]:visible absolute h-screen w-screen bg-black/70 -z-10 transition"
+            className="invisible data-[visible=true]:visible absolute h-screen w-screen bg-black/80 -z-10 transition"
             onClick={onClick}
         />
     );
