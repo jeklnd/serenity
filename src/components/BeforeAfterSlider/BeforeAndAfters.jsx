80ce4ab43bb18547bcd5bcdb29068abd34f01392
Jesse Krensel <jdkrensel@gmail.com>
2023-05-04 14:20:45 -0400

    remove unused state style How We Helped sections

--- src/components/BeforeAfterSlider/BeforeAndAfters.jsx
+++ src/components/BeforeAfterSlider/BeforeAndAfters.jsx
@@ -1,34 +1,24 @@
 import React from "react";
-import { useState } from "react";
 import Image from "next/image";
 import { couples } from "./data.js";
-import AccentBorder from "@/components/Contact/AccentBorder.jsx";
+import styles from "@/components/Services/ServiceBullets.module.css";
+import Link from "next/link";
 
 export default function BeforeAfterSlider() {
-    const [numberOfSlides, setNumberOfSlides] = useState(4);
-    const dots = () => {
-        const dotsArray = [];
-        for (let i = numberOfSlides; i > -1; i--) {
-            dotsArray.push(
-                <div
-                    key={i}
-                    className="bg-slate-300 rounded-full h-2 w-2"
-                ></div>
-            );
-        }
-        return dotsArray;
-    };
-    const heading_styles = "text-xl sm:text-xl md:text-2xl lg:text-4xl p-2 font-bold tracking-widest text-gray-700";
+    const heading_styles =
+        "text-xl sm:text-xl md:text-2xl lg:text-4xl p-2 font-bold tracking-widest text-gray-700";
     const beforeAfterListItems = couples.map((couple, index) => {
         return (
             <li key={index}>
-                <figure className="flex flex-col min-[400px]:flex-row justify-center gap-2 text-center  ">
+                <figure
+                    className={`${styles.bricks} flex flex-col min-[400px]:flex-row justify-center text-center`}
+                >
                     <div>
                         <h2 className={heading_styles}>BEFORE</h2>
                         <Image
                             src={couple.before}
                             alt=""
-                            className="rounded-sm "
+                            className="rounded-sm"
                         />
                     </div>
                     <div>
@@ -36,25 +26,34 @@ export default function BeforeAfterSlider() {
                         <Image
                             src={couple.after}
                             alt=""
-                            className="rounded-sm "
+                            className="rounded-sm"
                         />
                     </div>
                 </figure>
-                <figcaption className="text-lg sm:text-xl lg:text-2xl text-gray-700 p-2 max-w-4xl">
-                    <h3 className="text-xl sm:text-2xl lg:text-3xl font-medium color pt-2">How we helped</h3>
-                    <p className="text-gray-600">{couple.description}</p>
-                </figcaption>
+                <div className=" bg-slate-200 w-fullrounded-lg">
+                    <figcaption className="p-4 md:px-4 md:py-8 lg:px-0 max-w-4xl mx-auto flex flex-col gap-4">
+                        <h3 className="text-gray-600 text-xl sm:text-2xl lg:text-3xl xl:text-4xl font-medium color sm:text-center">
+                            How we helped
+                        </h3>
+                        <p className="text-gray-500 text-lg sm:text-xl lg:text-2xl ">
+                            {couple.description}{" "}
+                        </p>
+                        <button
+                            type="button"
+                            className="sm:self-center text-center w-fit font-extrabold text-gray-800 bg-sy-500 px-4 py-2 my-2 rounded-sm text-sm sm:text-md lg:text-lg border-t-2 border-sy-400/50 shadow-sm hover:shadow-md active:shadow-none"
+                        >
+                            <Link href="/contact">Request Service </Link>
+                        </button>
+                    </figcaption>
+                </div>
             </li>
         );
     });
     return (
         <div className="container mx-auto px-2 py-8 ">
             <div className="flex justify-around">
-                {/* <p>←</p> */}
                 <ul className="flex flex-col gap-12">{beforeAfterListItems}</ul>
-                {/* <p>→</p> */}
             </div>
-            {/* <div className="flex justify-center">{dots()}</div> */}
         </div>
     );
 }
