8c0a11ac8b373436b3d9276d9c0b866bc777d7c9
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 20:01:05 -0400

    style text add heading prop

--- src/components/RequestForm/FormSubHeading.jsx
+++ src/components/RequestForm/FormSubHeading.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function FormHeading() {
+export default function FormHeading({ heading }) {
     return (
-        <div className="flex flex-col text-center">
-            <p className="text-xl font-semibold text-gray-700 pb-0.5">
-                Request a complimentary quote
+        <div className="flex flex-col text-center gap-2">
+            <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-700">
+                {heading}
             </p>
-            <p className="text-sm text-gray-400 pb-2">
+            <p className="text-sm sm:text-lg text-gray-500 pb-2">
                 Submit your request and Serenity Home Repair will contact you
                 via phone or email in 24-48 hours.
             </p>
