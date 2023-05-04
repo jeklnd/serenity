ad109b808e603946093c81fdf9b5e6b3bd1361cc
Jesse Krensel <jdkrensel@gmail.com>
2023-05-04 14:22:53 -0400

    style: improve spacing and responsivity

--- src/components/Contact/ContactSection.jsx
+++ src/components/Contact/ContactSection.jsx
@@ -5,10 +5,13 @@ import ContactSectionForm from "./ContactSectionForm.jsx";
 export default function ContactSection() {
     return (
         <section className=" bg-slate-700 ">
-            <div className="bg-slate-400 w-screen flex justify-center relative overflow-hidden lg:py-16">
-                <div className="w-full relative flex flex-col gap-0 md:flex-row justify-center items-center z-10 p-2">
-                    <ContactInformation classesFromParent="pt-12 pb-6 sm:px-12 sm:py-12 w-fit lg:bg-slate-200 rounded-md text-gray-700 lg:text-gray-700 lg:px-14 lg:py-8 lg:shadow-md" />
-                    <ContactSectionForm classesFromParent="py-6 lg:py-8 w-fit shadow:sm rounded-md lg:relative lg:-mb-40 lg:-ml-8 bg-slate-300 lg:shadow-lg" heading="Request Quote"/>
+            <div className="bg-slate-400 w-screen flex justify-center relative overflow-hidden lg:py-16 md:p-2">
+                <div className="w-full relative flex flex-col md:flex-row justify-center items-center z-10 ">
+                    <ContactInformation classesFromParent="w-fit pt-12 pb-10 md:px-12 md:py-12 lg:px-12 lg:py-10  lg:bg-slate-200 rounded-md text-gray-700 lg:text-gray-700  lg:shadow-md" />
+                    <ContactSectionForm
+                        classesFromParent="px-2 py-4 lg:px-4 lg:py-6 lg:relative lg:-mb-44 lg:-ml-10 lg:shadow-lg xl:px-6 xl:py-8 w-fit shadow:sm rounded-md  bg-slate-300"
+                        heading="Request Quote"
+                    />
                 </div>
                 <div className="absolute bottom-0 w-[300vw] h-1/2 lg:h-full bg-slate-800 -rotate-[30deg] lg:-rotate-[15deg] translate-y-72 sm:max-lg:translate-x-12"></div>
             </div>
