a460bbe888102888c79802a1fcbc7a257ac1b4f2
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 20:09:02 -0400

    add responsive font sizing for heading remove unused code refactor component

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar.jsx";
-import ServicesBullets from "@/components/Services/ServicesBullets.jsx";
-import ContactInformation from "@/components/Contact/ContactInformation.jsx";
+import ServicesSection from "@/components/Services/ServicesSection.jsx";
 import ContactSection from "@/components/Contact/ContactSection.jsx";
 import NavFooter from "@/components/Footers/NavFooter.jsx";
 import CopyrightFooter from "@/components/Footers/CopyrightFooter.jsx";
@@ -12,12 +11,11 @@ export default function services() {
             <header className="sticky top-0 z-40">
                 <NavBar />
             </header>
-            <main className="grow flex flex-col bg-slate-200">
-                <h1 className="text-3xl font-bold text-gray-700 text-center pt-8">
+            <main className="grow flex flex-col bg-slate-400">
+                <h1 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-gray-700 text-center pt-14 -mb-12 z-10">
                     Services
                 </h1>
-                <ServicesBullets />
-                <ContactInformation />
+                <ServicesSection heading={""} />
                 <ContactSection />
             </main>
             <footer>
