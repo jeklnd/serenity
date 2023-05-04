180a41584b15d27c79be09b7e82b2db3ac6b4878
Jesse Krensel <jdkrensel@gmail.com>
2023-05-03 20:01:20 -0400

    style text size and bg color

--- src/components/RequestForm/RequestQuoteBtn.jsx
+++ src/components/RequestForm/RequestQuoteBtn.jsx
@@ -4,7 +4,7 @@ export default function RequestQuoteBtn({ text, oneOffClasses = "", onClick }) {
     return (
         <button
             type="submit"
-            className={`${oneOffClasses} w-fit font-extrabold text-gray-800 bg-[#ffc52f]/90 px-4 py-2 my-2 rounded-sm text-sm border-t-2 border-sy-400/50 shadow-sm hover:shadow-md active:shadow-none`}
+            className={`${oneOffClasses} w-fit font-extrabold text-gray-800 bg-sy-500 px-4 py-2 my-2 rounded-sm text-sm sm:text-md lg:text-lg border-t-2 border-sy-400/50 shadow-sm hover:shadow-md active:shadow-none`}
             onClick={onClick}
         >
             {text}
