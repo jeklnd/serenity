ae5e2a62d3e151fcd2f50c6a3f98f7e0ff1a2df3
Jesse Krensel <jdkrensel@gmail.com>
2023-05-05 12:00:46 -0400

    perf: optimize scripts with 'next/script'

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <script
+                <Script
                     type="text/javascript"
                     src="https://app.termly.io/embed.min.js"
                     data-auto-block="on"
