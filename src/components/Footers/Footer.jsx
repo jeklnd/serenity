0ba890296fd302b2849152fc3ab92e5a6b39bac3
Jesse Krensel <jdkrensel@gmail.com>
2023-05-05 10:11:43 -0400

    initial add

---
+++ src/components/Footers/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavigationFooter from "./NavigationFooter.jsx";
+import CopyrightFooter from "./CopyrightFooter.jsx";
+
+export default function Footer() {
+    return (
+        <footer>
+            <NavigationFooter />
+            <CopyrightFooter />
+        </footer>
+    );
+}
