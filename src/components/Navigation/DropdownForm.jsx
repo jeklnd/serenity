a6021da3c14b75081a36c859604a8a46d0fa4019
Jesse Krensel <jdkrensel@gmail.com>
2023-05-05 11:42:51 -0400

    refactor

---
+++ src/components/Navigation/DropdownForm.jsx
@@ -0,0 +1,28 @@
+import React, { useId } from "react";
+import RequestQuoteForm from "@/components/RequestForm/RequestQuoteForm.jsx";
+
+export default function DropdownForm({ isDropped, onClick }) {
+    const id = useId();
+
+    return (
+        <div>
+            <div
+                data-dropped={isDropped}
+                className=" sm:px-4 -translate-y-full data-[dropped=true]:translate-y-0 transition flex justify-center items-start bg-transparent h-fit w-screen absolute z-0 sm:bg-transparent"
+            >
+                <div className="py-6 sm:py-10 px-2 relative bg-gray-300 sm:my-6 sm:m-4 rounded-b-md sm:rounded-md">
+                    <RequestQuoteForm
+                        isInDropdown={true}
+                        id={id}
+                        heading="Request Quote"
+                    />
+                </div>
+            </div>
+            <div
+                data-dropped={isDropped}
+                className="invisible data-[dropped=true]:visible absolute h-screen w-screen bg-black/80 -z-10 transition"
+                onClick={onClick}
+            />
+        </div>
+    );
+}
