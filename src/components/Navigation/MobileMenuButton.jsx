ad6aba904291909cebefcc58da810d1d0d5d37ea
Jesse Krensel <jdkrensel@gmail.com>
2023-05-05 11:39:20 -0400

    rename

--- src/components/Navigation/MobileMenuButton.jsx
+++ src/components/Navigation/MobileMenuButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function MenuButton({onOpenMenu}) {
+export default function MobileMenuButton({onOpenMenu}) {
     return (
         <button type="button" className="flex flex-col gap-1" onClick={onOpenMenu}>
             <div className="h-0.5 w-4 bg-gray-600 sm:hidden"></div>
