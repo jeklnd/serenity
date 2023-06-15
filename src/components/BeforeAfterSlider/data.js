6b9f71b4f786e06275c9d549bd554b39188a7999
Jesse Krensel <jdkrensel@gmail.com>
2023-06-14 21:02:51 -0400

    add 2 before & afters

--- src/components/BeforeAfterSlider/data.js
+++ src/components/BeforeAfterSlider/data.js
@@ -1,3 +1,5 @@
+// All images must be 500 x 500 pixels
+
 import stepsBefore from "public/assets/beforeAndAfterImages/Scott_front-steps_before-scaled-puiwtl5x485xvvnuvcawkc94sqrojuowugszv3n894.jpg";
 import stepsAfter from "public/assets/beforeAndAfterImages/Scott_front-steps_after-scaled-puiwtdn7ljvnazys391w0e5g1nsqu9v25fl40vydmw.jpg";
 import sillBefore from "public/assets/beforeAndAfterImages/rotting-window-sill_before_2-scaled-puiwrphjg5lanye5uj1rktby10195n88l5u1b6fkp4.jpg";
@@ -6,30 +8,46 @@ import doorBefore from "public/assets/beforeAndAfterImages/Scott_screen-door-reb
 import doorAfter from "public/assets/beforeAndAfterImages/Scott_screen-door-rebuild_after-puiwpzg6x38ddow9ws8e08zitkj11mdycmrzmwzk3s.jpg";
 import houseBefore from "public/assets/beforeAndAfterImages/Rear-of-house_before-rotated-puiwq53823g3bco2zuo5f7kadvr8bt0cdeowikr72g.jpg";
 import houseAfter from "public/assets/beforeAndAfterImages/Rear-of-house_after-scaled-puiws985focbfrlhn9kxj6cmi3bynaelnvj8dzmb2g.jpg";
+import powerwashBefore from "public/assets/beforeAndAfterImages/powerwash_before.jpg";
+import powerwashAfter from "public/assets/beforeAndAfterImages/powerwash_after.jpg";
+import laundryBefore from "public/assets/beforeAndAfterImages/laundry_before.jpg";
+import laundryAfter from "public/assets/beforeAndAfterImages/laundry_after.jpg";
 
 export const couples = [
+    {
+        before: powerwashBefore,
+        after: powerwashAfter,
+        description:
+            "Years of exposure to the elements stained and discolored this brick patio, and a slimy green film of slippery algae and moss growth began to accumulate. Serenity Home Repair's expert powerwashing service removed the stains, eliminated the moss, and enlivened the look of the brickwork.",
+    },
+    {
+        before: laundryBefore,
+        after: laundryAfter,
+        description:
+            "The laundry room floor was ready for an affordable, visually-appealing upgrade. Serenity Home Repair's professional laminate flooring installation service enhanced the flooring's look and feel with durable, low-maintenance materials.",
+    },
     {
         before: stepsBefore,
         after: stepsAfter,
         description:
-            "The deck was covered in a thick layer of green sludge, making it dangerous to walk on. However, help arrived in the form of Serenity Home Repair's powerwashing services. With powerful jets of water, the grime was blasted away, leaving the deck looking clean, fresh, and safe once again.",
+            "The deck was covered in a thick layer of green sludge, making it dangerous to walk on. However, help arrived in the form of Serenity Home Repair's power washing services. With powerful jets of water, the grime was blasted away, leaving the deck looking clean, fresh, and safe once again.",
     },
     {
         before: sillBefore,
         after: sillAfter,
         description:
-            "The window sill had been neglected for years, causing it to rot and lose its paint. But with the arrival of Serenity Home Repairs, the damaged area was soon patched up and given a fresh coat of paint. Now the window sill looked as good as new, adding to the beauty of the home.",
+            "The window sill had been neglected for years, causing it to rot and lose its paint. But with the arrival of Serenity Home Repair, the damaged area was soon patched up and given a fresh coat of paint. Now the window sill looked as good as new, adding to the beauty of the home.",
     },
     {
         before: doorBefore,
         after: doorAfter,
         description:
-            "The door of the old house had seen better days, with mold growing on it and rusty hinges creaking with every movement. However, with a few simple repairs, the door was given a new lease of life. The hinges were replaced, the mold was scrubbed away, and a fresh coat of paint was applied, leaving the door looking as good as new.",
+            "The door of the old house had seen better days, with mold growing on it and rusty hinges creaking with every movement. However, with a few simple repairs, the door was given a new lease on life. The hinges were replaced, the mold was scrubbed away, and a fresh coat of paint was applied, leaving the door looking as good as new.",
     },
     {
         before: houseBefore,
         after: houseAfter,
         description:
-            "The fence beneath the house had broken segments, peeling paint, and significant portions missing. Serenity Home Repair came to the rescue, replacing the missing segments and repainting the fence to make it look as good as new. ",
+            "The fence beneath the house had broken segments, peeling paint, and significant portions missing. Serenity Home Repair came to the rescue, replacing the missing segments and repainting the fence with several coats of long-lasting paint.",
     },
 ];
