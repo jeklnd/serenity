fe2d7b568c0ead0ff37a5424ffba8bf0056e6b7a
Jesse Krensel <jdkrensel@gmail.com>
2023-06-14 21:03:37 -0400

    update style

--- src/components/Contact/ContactSection.jsx
+++ src/components/Contact/ContactSection.jsx
@@ -9,7 +9,7 @@ export default function ContactSection() {
             <div className="bg-slate-400 w-screen flex justify-center relative overflow-hidden lg:py-16 md:p-2">
                 <div className="w-full relative flex flex-col md:flex-row justify-center items-center z-10 ">
                     <ContactInformation classesFromParent="w-fit pt-12 pb-10 md:px-12 md:py-12 lg:px-12 lg:py-10  lg:bg-slate-200 rounded-md text-gray-700 lg:text-gray-700  lg:shadow-md" />
-                    <div className="flex items-center px-2 py-4 lg:px-4 lg:py-6 lg:relative lg:-mb-44 lg:-ml-10 lg:shadow-lg xl:px-6 xl:py-8 w-fit shadow:sm rounded-md  bg-slate-300">
+                    <div className="flex items-center px-2 py-4 lg:px-4 lg:py-6 lg:relative lg:-ml-8 lg:shadow-lg xl:px-6 xl:py-8 w-fit shadow:sm rounded-md  bg-slate-300">
                         <RequestQuoteForm id={id} heading="Request Quote" />
                     </div>
                 </div>
