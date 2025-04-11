6cab4c3735ff4e7444cad3b22e1c5f2cc9976699
Jesse Krensel <jdkrensel@gmail.com>
2025-04-11 00:03:01 -0400

    debug recaptcha site key

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function App({ Component, pageProps }) {
     const recaptchaSiteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
+    console.log("reCAPTCHA Site Key:", recaptchaSiteKey); // Log the key to check its value.
     
     return (
         <GoogleReCaptchaProvider reCaptchaKey={recaptchaSiteKey}>
