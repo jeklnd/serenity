813b1ae373222ecd59c2a6fe9b0b14d241fa6c4f
Jesse Krensel <jdkrensel@gmail.com>
2025-07-17 01:17:51 -0400

    letter visual clutter

--- src/components/RequestForm/FormSubHeading.jsx
+++ src/components/RequestForm/FormSubHeading.jsx
@@ -2,14 +2,11 @@ import React from "react";
 
 export default function FormHeading({ heading }) {
     return (
-        <div className="flex flex-col text-center gap-2">
+        <div className="flex flex-col text-center gap-2 pb-4">
             <p className="text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-700 font-['Fredoka_One']">
                 {heading}
             </p>
-            <p className="text-sm sm:text-lg text-gray-500 pb-2">
-                Submit your request and The Island Handyman will contact you
-                via phone or email in 1-2 business days.
-            </p>
+
         </div>
     );
 }
